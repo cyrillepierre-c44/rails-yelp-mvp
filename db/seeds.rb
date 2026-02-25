1064566c658e333aad126223215fd34658110eea
PIERRE Cyrille <cyrille.pierre@gmail.com>
2026-02-25 14:30:27 +0100

    corrections html terminées

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,11 @@ Restaurant.create!(name: "Dishoom", address: "7 Boundary St, London E2 7JE", pho
 puts "Created Dishoom"
 Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0618022452", category: "chinese")
 puts "Created Pizza East"
+Restaurant.create!(name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "0618022452", category: "french")
+puts "Created Dishoom"
+Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0618022452", category: "chinese")
+puts "Created Pizza East"
+Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0618022452", category: "chinese")
+puts "Created Pizza East"
 
 puts "Finished! Created #{Restaurant.count} restaurants."
